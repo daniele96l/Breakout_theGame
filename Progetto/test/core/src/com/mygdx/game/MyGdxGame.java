b1ba4eb0525a60e780a5f8ca1c1c9ed879d24f53
AlbertoGit30 <albertoschillaci30@gmail.com>
2018-04-06 15:09:00 +0200

    Introdotta prima forma di stati (più banale e primitiva possibile)

--- Progetto/test/core/src/com/mygdx/game/MyGdxGame.java
+++ Progetto/test/core/src/com/mygdx/game/MyGdxGame.java
@@ -1,4 +1,5 @@
 package com.mygdx.game;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import sprites.*;
 
@@ -20,7 +21,10 @@ public class MyGdxGame extends Game {
     public ArrayList<mattoncino> mattoncini = new ArrayList<mattoncino>();
     public  ball palla;
     public mattonella mattonella;
-    public Texture bg;
+    private Texture bg;
+    private Texture start;
+    private Texture gameOver;
+    private GameState gameState;
     public  int shift = 600;
 	
 	@Override
@@ -35,40 +39,66 @@ public class MyGdxGame extends Game {
             shift -= 150; //ogni mattoncino è distante dall'altro di uno shift
         }
         bg = new Texture("bg.jpg");
+        start=new Texture("start.jpg");
+        gameOver=new Texture("gameover.jpeg");
+        /*Immagini ovviamente temporanee (fanno cagare)*/
+        gameState=GameState.INIT;
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(bg, 0,0);
-        batch.draw(palla, palla.getPositionBall().x, palla.getPositionBall().y);
-        for(mattoncino mattoncino: mattoncini){
-            batch.draw(mattoncino, mattoncino.getPositionBrick().x, mattoncino.getPositionBrick().y);
-
-
-            //disegno i mattoncini
+		if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) { //Barra spaziatrice per iniziare
+		    if(gameState.equals(GameState.INIT)) {
+		        create();   //Soluzione TEMPORANEA, in attesa dell'implementazione dei livelli
+		        gameState=GameState.ACTION;
+            }
+            if(gameState.equals(GameState.GAME_OVER)) {
+		        gameState=GameState.INIT;
+            }
         }
+        batch.begin();
+        if(gameState.equals(GameState.ACTION)) {
+            batch.draw(bg, 0, 0);
+            batch.draw(palla, palla.getPositionBall().x, palla.getPositionBall().y);
+            for (mattoncino mattoncino : mattoncini) {
+                batch.draw(mattoncino, mattoncino.getPositionBrick().x, mattoncino.getPositionBrick().y);
 
-		batch.draw(mattonella, mattonella.getPosition().x,mattonella.getPosition().y);
-		mattonella.update(1);
-        int index = -1;
-        for(mattoncino mattoncino: mattoncini){
-            //dato che ho un arraylist devo aggiornare le condizioni dei mattoncini dentro un ciclo for
-            col = new Collision(mattoncino,palla);
-            if(col.check()){
-                index = mattoncini.indexOf(mattoncino);
+
+                //disegno i mattoncini
             }
 
-            //ovviamente anche l'aggiornamento del
+            batch.draw(mattonella, mattonella.getPosition().x, mattonella.getPosition().y);
+            mattonella.update(1);
+            int index = -1;
+            for (mattoncino mattoncino : mattoncini) {
+                //dato che ho un arraylist devo aggiornare le condizioni dei mattoncini dentro un ciclo for
+                col = new Collision(mattoncino, palla);
+                if (col.check()) {
+                    index = mattoncini.indexOf(mattoncino);
+                }
+
+                //ovviamente anche l'aggiornamento del
+            }
+            palla.update((float) 2, mattonella, mattoncino);
+            if (index != -1) {
+                mattoncini.remove(mattoncini.get(index));
+            }
+            if(palla.getPositionBall().y<=0) {
+                gameState=GameState.GAME_OVER;
+            }
         }
-        palla.update((float) 2, mattonella, mattoncino);
-        if(index != -1) {
-            mattoncini.remove(mattoncini.get(index));
+        else {
+		    if(gameState.equals(GameState.INIT)) {
+		        batch.draw(start,0,0);
+            }
+            if(gameState.equals(GameState.GAME_OVER)) {
+		        batch.draw(gameOver,0,0);
+            }
         }
-		batch.end();
-	}
+        batch.end();
+    }
 	
 	@Override
 	public void dispose () {
