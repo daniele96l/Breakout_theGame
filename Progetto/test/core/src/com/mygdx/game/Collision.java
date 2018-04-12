a7091d6bcb5ee79b355a9a6bdc68b75ae24ee73c
AlbertoGit30 <albertoschillaci30@gmail.com>
2018-04-12 21:24:17 +0200

    risoluzione "si spera" del problema degli urti

--- Progetto/test/core/src/com/mygdx/game/Collision.java
+++ Progetto/test/core/src/com/mygdx/game/Collision.java
@@ -12,8 +12,13 @@ public class Collision {
     private boolean topCollide;
     private double MAXBOUNCEANGLE = Math.PI/3;
     boolean top;
+    private Rectangle rectangle;
 
     public Collision(Brick mat, Ball palla){
+
+        int pallaX=(int)(palla.getBoundsBall().x+palla.getSpeedBall().x*Info.dt);
+        int pallaY=(int)(palla.getBoundsBall().y+palla.getSpeedBall().y*Info.dt);
+        rectangle=new Rectangle(pallaX,pallaY,palla.getWidth(), palla.getHeight());
         this.mat = mat;
         this.palla = palla;
         }
@@ -25,35 +30,40 @@ public class Collision {
     //questo metodo serve per la collisione con i brick verificando se collidono, nel caso cambio il verso della palla
     public boolean check(){
 
-        if(collidesTopBottom(palla.getBoundsBall())){ //controllo che collida col Brick
+        if(collidesTopBottom()){ //controllo che collida col Brick
+            if((palla.getSpeedBall().y*(palla.getBoundsBall().y-(mat.getBoundsBrick().y+mat.getBoundsBrick().height/2))>=0) && palla.getSpeedBall().y!=0) {
+                palla.getSpeedBall().set(-palla.getSpeedBall().x, palla.getSpeedBall().y);
+                delete();
+                return true;
+            }
             delete();            //quando abbiamo tanti mattoncini bisogna utilizzare un ciclo con un Arraylist
             palla.getSpeedBall().set(palla.getSpeedBall().x, -palla.getSpeedBall().y );
-            topCollide = true;
             return true;
         }
 
-        if(!topCollide)
-            if(collidesSide(palla.getBoundsBall())){
+        else {
+            if (collidesSide()) {
+                if((palla.getSpeedBall().x*(palla.getBoundsBall().x-(mat.getBoundsBrick().x+mat.getBoundsBrick().width/2))>=0) && palla.getSpeedBall().x!=0) {
+                    palla.getSpeedBall().set(palla.getSpeedBall().x, -palla.getSpeedBall().y );
+                    delete();
+                    return true;
+                }
                 delete();
                 palla.getSpeedBall().set(-palla.getSpeedBall().x, palla.getSpeedBall().y);
-                topCollide = true;
                 return true;
             }
+        }
 
-            return false;
+        return false;
     }
 
     //metodo usato per il check(quando tocca o sopra o sotto)
-    public boolean collidesTopBottom (Rectangle boundBall){
+    public boolean collidesTopBottom (){
         /////Controllo dove avviene l'impatto
-        if(boundBall.overlaps(mat.getBoundsBrick())) {
-            if ((boundBall.y-boundBall.height >= mat.getBoundsBrick().y)){
-                System.out.println("impact");
-                //impact from top
-                return true;
-            }
-            if(boundBall.y  <= mat.getBoundsBrick().y){
-                //impact from the bottom
+        if(mat.getBoundsBrick().overlaps(rectangle)) {
+
+            if ((palla.getBoundsBall().x+palla.getBoundsBall().width/2<=mat.getBoundsBrick().x+mat.getBoundsBrick().width)&&
+                    (palla.getBoundsBall().x+palla.getBoundsBall().width/2>=mat.getBoundsBrick().x)){
                 return true;
             }
         }
@@ -62,19 +72,14 @@ public class Collision {
 
 
     //metodo usato per il check(quando tocca a destra o sinistra)
-    public boolean collidesSide(Rectangle boundBall) { {
-            /////Controllo dove avviene l'impatto
-            if (boundBall.overlaps(mat.getBoundsBrick())) {
-                if (boundBall.x <=mat.getBoundsBrick().x) { //impact from the left
-                    return true;
-                }
-                if (boundBall.x >= mat.getBoundsBrick().x + mat.getBoundsBrick().width  -10 ) { //impact from the right
-                    return true;
-                }
-            }
-            return false;
+    public boolean collidesSide() {
+        /////Controllo dove avviene l'impatto
+        if (mat.getBoundsBrick().overlaps(rectangle)) {
+            return true;
         }
+        return false;
     }
+
 
     // questo metodo serve per controllare quando la palla collide con i bordi o con la Paddle
     public void checkside(Paddle paddle){
--- Progetto/test/core/src/com/mygdx/game/MyGdxGame.java
+++ Progetto/test/core/src/com/mygdx/game/MyGdxGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.math.Vector2;
 import sprites.*;
 
 
@@ -40,6 +41,7 @@ public class MyGdxGame extends Game implements TextInputListener {
     String text;
     TextInputListener textInputListener;
 	ChatClient chatClient = new ChatClient() ;
+	private ArrayList<Integer> indici;
 	@Override
 	public void create () {
         batch = new SpriteBatch();
@@ -89,7 +91,7 @@ public class MyGdxGame extends Game implements TextInputListener {
                 gameState=GameState.ACTION;
             }
         }
-        chatClient.start_main(palla.getPositionBall());
+ //       chatClient.start_main(palla.getPositionBall());
         if(nextLevel) {//deve stare dentro render perchè deve essere controllato sempre
             mattoncini = livello.selectLv();  //ritorno l'array adatto al nuovo livello
             bg = livello.getBg(); //reimposto il bg
@@ -100,7 +102,6 @@ public class MyGdxGame extends Game implements TextInputListener {
     }
 
     public void drawScene() {
-        int index = -1;
         batch.begin();
 
         if(gameState.equals(GameState.ACTION) ) {
@@ -121,18 +122,29 @@ public class MyGdxGame extends Game implements TextInputListener {
 
             player1.Move();     //mi permette di muovere il giocatore
 
-            for (Brick Brick : mattoncini) {
+            float oldSpeedBallX=palla.getSpeedBall().x;
+            float oldSpeedBallY=palla.getSpeedBall().y;
+
+            indici=new ArrayList<Integer>();
+            for (Brick brick : mattoncini) {
                 //dato che ho un arraylist devo aggiornare le condizioni dei mattoncini dentro un ciclo for
-                col = new Collision(Brick, palla);
+                col = new Collision(brick, palla);
                 if (col.check()) {
-                    index = mattoncini.indexOf(Brick);
+                    indici.add(mattoncini.indexOf(brick));
                 }
             }
 
             col.checkside(Paddle);
             //palla.update((float) 2, Paddle, Brick);
-            if (index != -1) {
-                mattoncini.remove(mattoncini.get(index));
+            if (!indici.isEmpty()) {
+                if(indici.size()==2) {
+                    palla.setSpeedBall(new Vector2(oldSpeedBallX,oldSpeedBallY));
+                    mattoncini.remove(Math.min(indici.get(0), indici.get(1)));
+                    mattoncini.remove(Math.min(indici.get(0), indici.get(1)));
+                }
+                else {
+                    mattoncini.remove((int)indici.get(0));
+                }
             }
             if(mattoncini.isEmpty()) {
                 gameState=GameState.YOU_WON;
