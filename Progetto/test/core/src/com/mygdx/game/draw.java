7ada0ebd0ba92f225167148cf77439d86bd3fcdd
daniele96l <daniele96ligato@gmail.com>
2018-04-13 16:09:29 +0200

    nuova classe draw

---
+++ Progetto/test/core/src/com/mygdx/game/draw.java
@@ -0,0 +1,26 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import help.Info;
+import sprites.Brick;
+import sprites.Paddle;
+
+import java.util.ArrayList;
+
+public class draw {
+
+
+    public void disegnare(SpriteBatch batch, ArrayList<Brick> mattoncini, Paddle Paddle) {
+
+        for (Brick Brick : mattoncini) {
+            batch.draw(Brick, Brick.getPositionBrick().x, Brick.getPositionBrick().y,Brick.getWidth()* Info.brickresize,Brick.getHeight()* Info.brickresize);
+            //disegno i mattoncini
+        }
+
+
+        batch.draw(Paddle, Paddle.getPosition().x, Paddle.getPosition().y, Paddle.getWidth()* Info.paddleresize , Paddle.getHeight()*Info.paddleresize);
+
+
+
+    }
+}
