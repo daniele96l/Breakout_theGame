6be50aca8459e3f7990bc12ddc2ba551ed1229b3
AlbertoGit30 <albertoschillaci30@gmail.com>
2018-04-20 15:17:35 +0200

    ora i mattoncini sfruttano l'ereditarietà

--- Progetto/test/core/src/sprites/Brick/AbstractBrick.java
+++ Progetto/test/core/src/sprites/Brick/AbstractBrick.java
@@ -11,16 +11,11 @@ public  abstract class AbstractBrick  extends Sprite{
     protected Vector2 positionBrick;
     protected Rectangle boundsBrick;
     protected boolean eliminato;
-    protected int durezza;
 
     public AbstractBrick(int posX, int posY, String nome){
         super(new Texture(nome));
         positionBrick = new Vector2(posX, posY);
         boundsBrick = new Rectangle(posX, posY, this.getWidth()* Info.brickresize, this.getHeight()* Info.brickresize );
-    }
-
-    public int getDurezza() {
-        return durezza;
     }
 
     public Vector2 getPositionBrick() {
--- Progetto/test/core/src/sprites/Brick/HardBrick.java
+++ Progetto/test/core/src/sprites/Brick/HardBrick.java
@@ -8,40 +8,7 @@ public class HardBrick extends AbstractBrick {
 
     public HardBrick(int posX, int posY){
         super(posX, posY, "brick.jpg");
-        this.durezza = 1;
         //il boundBrick si riferiscono al rettangolo invisibile che costruirò intorno alla texture
         //l'altezza e la larghezza sono della texture
-    }
-
-    public int getDurezza() {
-        return durezza;
-    }
-
-    public Vector2 getPositionBrick() {
-        return positionBrick;
-    }
-
-    public void delete(){
-        eliminato = true;
-    }
-
-    public void setPositionBrick(Vector2 positionBrick) {
-        this.positionBrick = positionBrick;
-    }
-
-    public void setBoundsBrick(Rectangle boundsBrick) {
-        this.boundsBrick = boundsBrick;
-    }
-
-    public void setEliminato(boolean eliminato) {
-        this.eliminato = eliminato;
-    }
-
-    public Rectangle getBoundsBrick() {
-        return boundsBrick;
-    }
-
-    public boolean isEliminato() {
-        return eliminato;
     }
 }
