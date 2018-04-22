9c280a0eb3e709813c5434a2b90e393e83507516
regnacristian <32876104+regnacristian@users.noreply.github.com>
2018-04-22 15:37:32 +0200

    Menu funzionante e centrati meglio i bottoni, risolto bug della pausa con tasto menu e multiplayer

--- Progetto/test/core/src/com/mygdx/game/State/MainMenuState.java
+++ Progetto/test/core/src/com/mygdx/game/State/MainMenuState.java
@@ -35,33 +35,30 @@ public class MainMenuState  {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.draw(menu, 0, 0);
-        batch.draw(playButton, Info.larghezza / 2 - playButton.getWidth() / 2, 530);//alpostodimetterlicosipossousaredellecostanti
-        batch.draw(exitButton, Info.larghezza / 2 - exitButton.getWidth() / 2, 150);
-        batch.draw(multiplayerButton, Info.larghezza / 2 - multiplayerButton.getWidth() / 2, 350);//immaginibruttissime
-        batch.draw(score,Info.larghezza / 2 - multiplayerButton.getWidth() / 2, 700 );
-        if (Gdx.input.getX() > Info.larghezza / 2 - score.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + score.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 700 && (Info.altezza - Gdx.input.getY() < 700 + score.getHeight()))) {
+        batch.draw(playButton, Info.larghezza / 2 - playButton.getWidth() / 2, 620);//alpostodimetterlicosipossousaredellecostanti
+        batch.draw(exitButton, Info.larghezza / 2 - exitButton.getWidth() / 2, 140);
+        batch.draw(multiplayerButton, Info.larghezza / 2 - multiplayerButton.getWidth() / 2, 460);//immaginibruttissime
+        batch.draw(score,Info.larghezza / 2 - multiplayerButton.getWidth() / 2, 300 );
+        if (Gdx.input.getX() > Info.larghezza / 2 - score.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + score.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 300 && (Info.altezza - Gdx.input.getY() < 300 + score.getHeight()))) {
             if (Gdx.input.isTouched())
                 return GameState.SCORE;
         }
 
-        if (Gdx.input.getX() > Info.larghezza / 2 - playButton.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + exitButton.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 150 && (Info.altezza - Gdx.input.getY() < 150 + exitButton.getHeight()))) {
+        if (Gdx.input.getX() > Info.larghezza / 2 - playButton.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + exitButton.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 140 && (Info.altezza - Gdx.input.getY() < 140 + exitButton.getHeight()))) {
             if (Gdx.input.isTouched())
                 Gdx.app.exit();
         }
-        if (Gdx.input.getX() > Info.larghezza / 2 - playButton.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + playButton.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 550 && (Info.altezza - Gdx.input.getY() < 550 + exitButton.getHeight()))) {
+        if (Gdx.input.getX() > Info.larghezza / 2 - playButton.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + playButton.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 620 && (Info.altezza - Gdx.input.getY() < 620 + exitButton.getHeight()))) {
             if (Gdx.input.isTouched()) {
                 return GameState.ACTION;
             }
         }
-        if (Gdx.input.getX() > Info.larghezza / 2 - multiplayerButton.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + multiplayerButton.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 350 && (Info.altezza - Gdx.input.getY() < 350 + multiplayerButton.getHeight()))) {
-            if (Gdx.input.isTouched()) {
+        if (Gdx.input.getX() > Info.larghezza / 2 - multiplayerButton.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + multiplayerButton.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 460 && (Info.altezza - Gdx.input.getY() < 460 + multiplayerButton.getHeight()))) {
+            if (Gdx.input.justTouched()) {
                 return GameState.MULTIPLAYER;
             }
         }
         return GameState.MENU;
     }
-
-
-
 
 }
