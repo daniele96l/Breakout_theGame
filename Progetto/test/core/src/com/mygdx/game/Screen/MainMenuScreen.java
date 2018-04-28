0336d38504539a3eb432ae89a4483ab9201e466f
regnacristian <32876104+regnacristian@users.noreply.github.com>
2018-04-28 10:46:13 +0200

    Aggiunto tasto per multiplayer offline e online

--- Progetto/test/core/src/com/mygdx/game/Screen/MainMenuScreen.java
+++ Progetto/test/core/src/com/mygdx/game/Screen/MainMenuScreen.java
@@ -13,8 +13,9 @@ public class MainMenuScreen implements Screen {
     private Texture menu;
     private Texture playButton;
     private Texture exitButton;
-    private Texture multiplayerButton;
+    private Texture multiplayerofflineButton;
     private Texture score;
+    private Texture multiplayeronlineButton;
     BreakGame game;
     private int numeroPlayer;
 
@@ -28,7 +29,8 @@ public class MainMenuScreen implements Screen {
         menu = new Texture("menuscreen.jpg");
         playButton = new Texture("play.png");
         exitButton = new Texture("exit.png");
-        multiplayerButton = new Texture("multiplayer.png");
+        multiplayerofflineButton = new Texture("multiplayeroffline.png");
+        multiplayeronlineButton = new Texture("multiplayeronline.png");
         score = new Texture("score.png");
 
 
@@ -42,33 +44,41 @@ public class MainMenuScreen implements Screen {
 
         game.getBatch().begin();
         game.getBatch().draw(menu, 0, 0);
-        game.getBatch().draw(playButton, Info.larghezza / 2 - playButton.getWidth() / 2, 620);//alpostodimetterlicosipossousaredellecostanti
-        game.getBatch().draw(exitButton, Info.larghezza / 2 - exitButton.getWidth() / 2, 140);
-        game.getBatch().draw(multiplayerButton, Info.larghezza / 2 - multiplayerButton.getWidth() / 2, 460);//immaginibruttissime
-        game.getBatch().draw(score,Info.larghezza / 2 - multiplayerButton.getWidth() / 2, 300 );
+        game.getBatch().draw(playButton, Info.larghezza / 2 - playButton.getWidth() / 2, 650);//alpostodimetterlicosipossousaredellecostanti
+        game.getBatch().draw(exitButton, Info.larghezza / 2 - exitButton.getWidth() / 2, 100);
+        game.getBatch().draw(multiplayerofflineButton, Info.larghezza / 2 - multiplayerofflineButton.getWidth() / 2, 520);//immaginibruttissime
+        game.getBatch().draw(score,Info.larghezza / 2 - score.getWidth() / 2, 240 );
+        game.getBatch().draw(multiplayeronlineButton,Info.larghezza / 2 - multiplayeronlineButton.getWidth() / 2, 380 );
 
-        if (Gdx.input.getX() > Info.larghezza / 2 - score.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + score.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 300 && (Info.altezza - Gdx.input.getY() < 300 + score.getHeight()))) {
+        if (Gdx.input.getX() > Info.larghezza / 2 - score.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + score.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 240 && (Info.altezza - Gdx.input.getY() < 240 + score.getHeight()))) {
             if (Gdx.input.justTouched())
                 game.setScreen(new ScoreScreen(game));
         }
 
-        if (Gdx.input.getX() > Info.larghezza / 2 - playButton.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + exitButton.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 140 && (Info.altezza - Gdx.input.getY() < 140 + exitButton.getHeight()))) {
+        if (Gdx.input.getX() > Info.larghezza / 2 - playButton.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + exitButton.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 100 && (Info.altezza - Gdx.input.getY() < 100 + exitButton.getHeight()))) {
             if (Gdx.input.justTouched())
                 Gdx.app.exit();
         }
-        if (Gdx.input.getX() > Info.larghezza / 2 - playButton.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + playButton.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 620 && (Info.altezza - Gdx.input.getY() < 620 + exitButton.getHeight()))) {
+        if (Gdx.input.getX() > Info.larghezza / 2 - playButton.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + playButton.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 650 && (Info.altezza - Gdx.input.getY() < 650 + exitButton.getHeight()))) {
             if (Gdx.input.justTouched()) {
                 OfflineGameScreen.setPlayerName(JOptionPane.showInputDialog(null, "Enter a nickname", "Nickname ", 1));
                 game.setScreen(new OfflineGameScreen(game,1));
             }
         }
-        if (Gdx.input.getX() > Info.larghezza / 2 - multiplayerButton.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + multiplayerButton.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 460 && (Info.altezza - Gdx.input.getY() < 460 + multiplayerButton.getHeight()))) {
+        if (Gdx.input.getX() > Info.larghezza / 2 - multiplayerofflineButton.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + multiplayerofflineButton.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 520 && (Info.altezza - Gdx.input.getY() < 520 + multiplayerofflineButton.getHeight()))) {
             if (Gdx.input.justTouched()) {
                 OfflineGameScreen.setPlayerName(JOptionPane.showInputDialog(null, "Enter a nickname", "Nickname ", 1));
                 numeroPlayer =(Integer.parseInt(JOptionPane.showInputDialog(null, "Number of player", "Enter the number of player ", 1)));
                 game.setScreen(new OfflineGameScreen(game,numeroPlayer));
             }
         }
+
+        if (Gdx.input.getX() > Info.larghezza / 2 - multiplayeronlineButton.getWidth() / 2 && (Gdx.input.getX() < Info.larghezza / 2 + multiplayeronlineButton.getWidth() / 2) && (Info.altezza - Gdx.input.getY() > 380 && (Info.altezza - Gdx.input.getY() < 380 + multiplayeronlineButton.getHeight()))) {
+            if (Gdx.input.justTouched()) {
+                //game.setScreen();      Qua si mettera il MultiplayerGameScreen
+            }
+        }
+
 
 
         game.getBatch().end();
