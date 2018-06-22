5655a8cde6728643be480d9fc5081ff417171592
Cotogni <marco.cotogni01@universitadipavia.it>
2018-06-22 21:53:36 +0200

    Test on Timer

--- Progetto/test/core/src/com/mygdx/game/help/Timer.java
+++ Progetto/test/core/src/com/mygdx/game/help/Timer.java
@@ -16,12 +16,17 @@ public class Timer {
      * @param date
      * @param numeroPlayer
      */
-    public void checkTimer(ArrayList<Date> date, int numeroPlayer){
+    public boolean checkTimer(ArrayList<Date> date, int numeroPlayer){
         if(date != null){
             Date date2 = new Date();
             for(int i =0; i < numeroPlayer;i++)
                 if(date2.getTime() - date.get(i).getTime() > Info.getInstance().getDurataPowerUp())
+                {
                     Info.getInstance().getPaddleresizex().set(i,Info.getInstance().getPaddleresize());
+                    return true;
+                }
+                return false;
         }
+        return false;
     }
 }
